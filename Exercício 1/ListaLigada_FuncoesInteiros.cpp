1b8a1b8930a3ee708324f2802c9ef1967e808dfc
Alexander Fortes <fortesalexg@hotmail.com>
2021-08-06 21:59:08 +0100

    Versão 2

--- Exercício 1/ListaLigada_FuncoesInteiros.cpp
+++ Exercício 1/ListaLigada_FuncoesInteiros.cpp
@@ -71,14 +71,143 @@ using namespace std;
 	// função  remove_by_index() -> Retira um elemento de uma lista ligada através da sua posição na lista.
 	
 	int remove_by_value(node_list**, int);
-	// função remove_by_value() -> Retira um elemento de uma lista ligada através do seu valor (primeira ocorrancia).
+	// função remove_by_value() -> Retira um elemento de uma lista ligada através do seu valor (primeira ocorrencia).
 	
 	int remove_by_valueAll(node_list**, int);
-	// função remove_by_valueAll
-	- > Retira um elemento de uma lista ligada através   do seu valor (todas as ocorrancias)
-	void delete list (node  
-	// * Fungäo "delete list ( ) "
-		// * 	Elimina toda a lista ligada .
+	// função remove_by_valueAll -> Retira um elemento de uma lista ligada através do seu valor (todas as ocorrencias)
+	
+	void delete_list(node_list**);  
+	// função delete_list() -> Elimina toda a lista ligada.
+	
+// função size() -> devolve o número de elementos da lista ligada	
+
+	// Entrada:
+//	node_list*head -> ponteiro para o início da lista ligada
+
+	// Saida
+// número da estutura de dados ou zero se a lesta não existir.
+
+	int size(node_list*head){
+		node_list *aux = NULL;
+	
+	if (head == NULL)	// Lista vazia.
+	return 0;
+	else
+	{
+	int numE=0; aux = head; 
+	while (aux . = NULL){
+		numE ++ ; aux = aux->next; 
+	}
+	return numE;
+} // Fim do else.
+} // Fim da função size()
+	 
+	void pushfirst (node_list ** head, int val) {
+	
+		node_list * new_node; 
+		new_node = (node_list*)malloc(sizeof(node_list));
+		
+		new_node->val = val;
+		new_node->next = *head;
+		*head = new_node; 
+	}// Fim da função
+	
+	void push(node_list ** head, int val) {
+	// Dados 
+	node_list * current = *head;
+	// Ponteiro que aponta para a última posição da lista ligada.
+	// É inicializado para o inicio da lista .
+	
+	// Algoritmo
+	// 1. lista vazia
+	if (size(*head)==0)
+	{
+		pushfirst(head,val);
+	}
+	// 2. Obtém o último elemento da lista
+	while (current->next != NULL) {
+	current = current->next;
+}
+	// 3. Insere um novo elemento na última posição 
+	current->next = (node_list*)malloc(sizeof(node_list));
+	current->next->val  = val;
+	current->next->next = NULL;
+}
+	
+	// função add_by_index () -> Insere um elemento numa determinada posição de uma lista ligada
+	 
+	// Entrada(s): 
+	//	node_list ** head	-> ponteiro para ponteiro para o inicio da lista ligada.
+	//						-> posição.
+	//						-> valor a inserir
+	
+	//  Saida(s): devolve valor inserido ou -1 em caso de erro.
+	 
+	int add_by_index(node_list * *head, int n, int val) {
+	// 1. Dados
+		int i=0;
+		int retval = -1;
+		node_list * before = *head;
+		node_list * after = NULL;
+		
+	// 2. Algoritmo
+	if (*head == NULL)
+	{
+		cout<<"Lista vazia!"<<endl;
+		return -1;
+	}
+	else
+	if(n == 0){
+		pushtirst(head, val);
+		cout<<"Elemento inserido na primeira posição."<<endl;
+		return val;
+	}
+	else if (n > size(*head))
+	{
+		cout<<"Posições começam de 0."<<endl;
+		cout<<"Estrutura só tem "<<size(*head)<<"elementos."<<endl;
+		return -1;
+	}
+	else if(n == size(*head))
+	{
+		push(head,val);
+		return val;
+	}
+	else{
+		for (i = 0; i < n; i++){
+			if (i>=1)
+			before = before->next;
+			
+				if (before->next != NULL)
+					after = before->next;
+		}
+	// Definição do novo elemento 
+	node_list * new_node;
+	new_node = (node_list*) malloc(sizeof(node_list));
+	new_node->val = val; 
+	new_node->next = NULL;
+	
+	// Inserção do novo elemento entre "before" e "after" 
+	before->next = new_node;
+	new_node->next = after; 
+	return retval;
+}
+} // fim da função
+ 
+// * Fungäo "print List ( ) "
+	// * 	Apresenta os elementos de uma lista ligada
+( linked list")
+ 
+// * Entrada (s) .
+node list * head - > ponteiro para o inicio da lista
+ligada .
+// * Saida (s) : näo tem.
+ 
+void print list (node list  *  head) { // Declaraqäo de um ponteiro
+/ / que vai percorrendo a 1 is ta node list * current = NULL ; curren t=head ;
+
+
+
 	 
 	
 
